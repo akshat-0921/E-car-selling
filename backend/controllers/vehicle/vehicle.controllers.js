f66eba4c21eabfb9389de8008e7be988cecfe230
akshat-0921 <akshat21lko@gmail.com>
2025-01-20 00:09:22 +0530

    correcting vehicle controller

--- backend/controllers/vehicle/vehicle.controllers.js
+++ backend/controllers/vehicle/vehicle.controllers.js
@@ -2,14 +2,15 @@ import { Vehicle } from "../../models/vehicle.models.js";
 import { Brand } from "../../models/brand.models.js";
 import { Showroom } from "../../models/showroom.models.js";
 import { ShowroomVehicle } from "../../models/showroomVehicle.models.js";
+import { User } from "../../models/user.models.js";
 
 const addVehicle = async (req, res) => {
    try {
-      const brandId = req.params?._id;
+      const brandId = req.params._id;
       const { name, category, price, year } = req.body;
       const { engine, performance, transmission, dimensions, safetyFeatures, connectivity, warranty, customisation } = req.body;
-      const brand = await Brand.findById(brandId)
-      if (!brand) { return res.status(404).json({ success: false, msg: "Brand does not exist" }) }
+      const brand = await Brand.findById(brandId);
+      if (!brand) { return res.status(404).json({ success: false, msg: "Brand does not exist" }); }
       const vehicle = await Vehicle.create({
          name,
          category,
@@ -24,7 +25,8 @@ const addVehicle = async (req, res) => {
          connectivity,
          warranty,
          customisation,
-         buyers: []
+         buyers: [],
+         showrooms: []
       });
 
       return res.status(201).json({ success: true, msg: "Vehicle added to database", vehicle });
@@ -50,22 +52,21 @@ const getAllVehicles = async (req, res) => {
 const getVehicle = async (req, res) => {
    try {
       const vehicleId = req.params._id;
-      // if (!vehicleId) return res.status(400).json({ success: false, msg: "brandId required" })
       const vehicle = await Vehicle.findById(vehicleId);
-      // if (!vehicle) {
-      return res.status(404).json({ success: false, msg: "Vehicle does not exist" });
+      if (!vehicle) {
+         return res.status(404).json({ success: false, msg: "Vehicle does not exist" });
+      }
+      return res.status(200).json({ success: true, msg: "Vehicle found", vehicle });
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json({ success: false, msg: "An error occurred while fetching the vehicle. Please try again" });
    }
-      return res.status(200).json(200).json({ success: true, msg: "Vehicle found", vehicle });
-} catch (error) {
-   console.log(error);
-   return res.status(500).json({ success: false, msg: "An error occurred while fetching the vehicle. Please try again" });
-}
 };
 
 const updateVehicle = async (req, res) => {
    try {
       const vehicleId = req.params._id;
-      if (!req.body) { return res.status(400).json({ success: false, msg: "Enter some fields" }) }
+      if (!req.body) { return res.status(400).json({ success: false, msg: "Enter some fields" }); }
 
       const { name, category, price, year } = req.body;
       const { engine, performance, transmission, dimensions, safetyFeatures, connectivity, warranty, customisation } = req.body;
@@ -90,7 +91,7 @@ const updateVehicle = async (req, res) => {
                safetyFeatures: safetyFeatures || vehicle.safetyFeatures,
                connectivity: connectivity || vehicle.connectivity,
                warranty: warranty || vehicle.warranty,
-               customisation: customisation || vehicle.customisation,
+               customisation: customisation || vehicle.customisation
             }
          },
          { new: true }
@@ -110,23 +111,18 @@ const deleteVehicle = async (req, res) => {
          return res.status(404).json({ success: false, msg: "Vehicle does not exist" });
       }
 
-      const showrooms = vehicle.showrooms;
-
       await Promise.all(
          vehicle.showrooms.map(async (showroomId) => {
-            const showroom = await Showroom.findByIdAndUpdate(
+            await Showroom.findByIdAndUpdate(
                { _id: showroomId },
                { $pull: { vehicles: vehicleId } },
                { new: true }
             );
-            if (!showroom) { return res.status(404).json({ success: false, msg: "Showroom not found" }) }
          })
       );
 
       const brandId = vehicle.brandId;
-      const updateBrand = await Brand.findByIdAndUpdate({ _id: brandId }, { $pull: { vehicles: vehicleId } });
-      if (!updateBrand) { return res.status(404).json({ success: false, msg: "Brand not found" }) }
-
+      await Brand.findByIdAndUpdate({ _id: brandId }, { $pull: { vehicles: vehicleId } });
       await Vehicle.findByIdAndDelete(vehicleId);
 
       return res.status(200).json({ success: true, msg: "Vehicle deleted successfully" });
@@ -140,12 +136,11 @@ const insertVehicleToShowroom = async (req, res) => {
    try {
       const vehicleId = req.params._id;
       const { showroomId } = req.body;
-      if (!showroomId) { return res.status(400).json({ success: false, msg: "Showroom id required" }) }
-      const vehicle = await Vehicle.findByIdAndUpdate(vehicleId, { $push: { showrooms: showroomId } });
-      const showroom = await Showroom.findByIdAndUpdate(showroomId, { $push: { vehicles: vehicleId } });
-      if (!showroom) { return res.status(404).json({ success: false, msg: "Showroom does not exist" }) }
-      const showroomVehicle = await ShowroomVehicle.create({ showroomId, vehicleId })
-      return res.status(200).json({ success: true, msg: `Vehicle ${vehicle.name} added to showroom ${showroom.name}` });
+      if (!showroomId) { return res.status(400).json({ success: false, msg: "Showroom id required" }); }
+      const vehicle = await Vehicle.findByIdAndUpdate(vehicleId, { $addToSet: { showrooms: showroomId } });
+      const showroom = await Showroom.findByIdAndUpdate(showroomId, { $addToSet: { vehicles: vehicleId } });
+      await ShowroomVehicle.create({ showroomId, vehicleId });
+      return res.status(200).json({ success: true, msg: `Vehicle added to showroom` });
    } catch (error) {
       console.log(error);
       return res.status(500).json({ success: false, msg: "An error occurred while adding vehicle to showroom. Please try again" });
@@ -156,13 +151,11 @@ const removeVehicleFromShowroom = async (req, res) => {
    try {
       const vehicleId = req.params._id;
       const { showroomId } = req.body;
-      if (!showroomId) { return res.status(400).json({ success: false, msg: "Showroom id required" }) }
+      if (!showroomId) { return res.status(400).json({ success: false, msg: "Showroom id required" }); }
       const vehicle = await Vehicle.findByIdAndUpdate(vehicleId, { $pull: { showrooms: showroomId } });
       const showroom = await Showroom.findByIdAndUpdate(showroomId, { $pull: { vehicles: vehicleId } });
-      if (!showroom) { return res.status(404).json({ success: false, msg: "Showroom does not exist" }) }
-      const deleteShowroomVehilce = await ShowroomVehicle.findOneAndDelete(showroomId, vehicleId)
-      if (!deleteShowroomVehilce) { return res.status(400).json({ success: false, msg: "Vehicle could not be removed from showroom" }) }
-      return res.status(200).json({ success: true, msg: `Vehicle ${vehicle.name} removed from showroom ${showroom.name}` });
+      await ShowroomVehicle.findOneAndDelete({ showroomId, vehicleId });
+      return res.status(200).json({ success: true, msg: `Vehicle removed from showroom` });
    } catch (error) {
       console.log(error);
       return res.status(500).json({ success: false, msg: "An error occurred while removing vehicle from showroom. Please try again" });
@@ -175,7 +168,7 @@ const purchaseVehicle = async (req, res) => {
 
       const vehicle = await Vehicle.findByIdAndUpdate(
          vehicleId,
-         { $push: { buyers: userId } },
+         { $addToSet: { buyers: userId } },
          { new: true }
       );
 
@@ -183,9 +176,15 @@ const purchaseVehicle = async (req, res) => {
          return res.status(404).json({ success: false, msg: "Vehicle not found" });
       }
 
+      await User.findByIdAndUpdate(
+         userId,
+         { $addToSet: { purchasedVehicles: vehicleId } },
+         { new: true }
+      );
+
       return res.status(200).json({
          success: true,
-         msg: "Vehicle purchased successfully and buyer added!",
+         msg: "Vehicle purchased successfully and buyer added",
          vehicle
       });
    } catch (error) {
@@ -202,5 +201,5 @@ export {
    deleteVehicle,
    insertVehicleToShowroom,
    removeVehicleFromShowroom,
-   purchaseVehicle,
+   purchaseVehicle
 };
