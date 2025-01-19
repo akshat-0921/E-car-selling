c8ccde88b413dbd47b9b2065dbe63b17dc84d747
akshat-0921 <akshat21lko@gmail.com>
2025-01-19 17:56:15 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,34 +7,34 @@ import connectDB from "./config/mongodb.js";
 const app = express();
 const port = 4000;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
+
+// Database connection
 connectDB();
 
+// Import routes
 import brandRouter from "./routes/brand.routes.js";
 import showroomRouter from "./routes/showroom.routes.js";
 import userRoutes from "./routes/user.routes.js";
-<<<<<<< HEAD
 import adminRoutes from "./routes/admin.routes.js";
-=======
 import vehicleRouter from "./routes/vehicle.routes.js";
->>>>>>> a04544ca6b7f96ec20c5638a729290eb368b2ae0
 
 // Routes
 app.use("/api/brand", brandRouter);
 app.use("/api/showroom", showroomRouter);
 app.use("/api/user", userRoutes);
-<<<<<<< HEAD
 app.use("/api/admin", adminRoutes);
-=======
 app.use("/api/vehicle", vehicleRouter);
->>>>>>> a04544ca6b7f96ec20c5638a729290eb368b2ae0
 
+// Root route
 app.get("/", (req, res) => {
    console.log("API Working");
    res.status(200).send("API is working");
 });
 
+// Start the server
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
