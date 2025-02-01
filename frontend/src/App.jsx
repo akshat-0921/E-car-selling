bab69cc209f9bed2cc118d36766f18d705f427b3
AnkitShome <ankitshome7@gmail.com>
2025-02-01 16:18:26 +0530

    Auth component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import LoginForm from "./components/Auth/LoginForm";
 import SignUpForm from "./components/Auth/SignUpForm";
+import Auth from "./components/Auth/Auth";
 
 function App() {
    return (
@@ -11,6 +12,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<LoginForm />} />
             <Route path="/signup" element={<SignUpForm />}></Route>
+            <Route path="/auth" element={<Auth />}></Route>
          </Routes>
       </Router>
    );
---
+++ frontend/src/components/Auth/Auth.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import SignUpForm from "./SignUpForm";
+import LoginForm from "./LoginForm";
+
+const Auth = () => {
+   const [isSign, setIsSign] = useState(true);
+
+   return (
+      <div className="flex flex-col justify-center gap-0 items-center min-h-screen bg-gray-100 py-12">
+         {/* Button Container */}
+         <div className="flex space-x-8">
+            <button
+               onClick={() => setIsSign(true)}
+               className={`px-8 py-3 rounded-md shadow-md font-semibold transition-all duration-300 ${isSign ? "bg-blue-600 text-white" : "bg-gray-300"}`}
+            >
+               Sign Up
+            </button>
+            <button
+               onClick={() => setIsSign(false)}
+               className={`px-8 py-3 rounded-md shadow-md font-semibold transition-all duration-300 ${!isSign ? "bg-blue-600 text-white" : "bg-gray-300"}`}
+            >
+               Login
+            </button>
+         </div>
+
+         {/* Render Forms */}
+         <div className="w-full max-w-lg px-6 bg-gray-100 rounded-md">
+            {isSign ? <SignUpForm /> : <LoginForm />}
+         </div>
+      </div>
+   );
+};
+
+export default Auth;
