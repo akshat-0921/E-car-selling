a53106a377b2ebc556113cbee78f07162bd66b19
AnkitShome <ankitshome7@gmail.com>
2025-05-28 19:15:25 +0530

    Brand wise car fetching functionality added

--- frontend/src/pages/Brands/BrandDetails.jsx
+++ frontend/src/pages/Brands/BrandDetails.jsx
@@ -1,33 +1,17 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchVehiclesByBrandThunk } from '../../redux/brandSlice';
+import React from 'react';
 import { useParams } from 'react-router-dom';
+import Vehicle from '../Vehicle/Vehicle'; // Adjust import path as needed
 
 const BrandDetails = () => {
-
    const { brandId } = useParams();
-   const dispatch = useDispatch();
-   const { vehicles, brandLoading, brandError } = useSelector((state) => state.vehicle)
-
-   useEffect(() => {
-      if (brandId) {
-         dispatch(fetchVehiclesByBrandThunk(brandId))
-      }
-   }, [dispatch, brandId])
-
-   if (brandLoading) return <p>Loading...</p>;
-   if (brandError) return <p className="text-red-500">{brandError}</p>;
 
    return (
       <div>
          <h2 className="text-2xl font-semibold mb-6">Vehicles under this Brand</h2>
-         <ul>
-            {vehicles.map(vehicle => (
-               <li key={vehicle._id}>{vehicle.name}</li>
-            ))}
-         </ul>
+         {/* Pass brandId as the brand prop to Vehicle */}
+         <Vehicle brandId={brandId} />
       </div>
    );
-}
+};
 
-export default BrandDetails
+export default BrandDetails;
--- frontend/src/pages/Vehicle/Vehicle.jsx
+++ frontend/src/pages/Vehicle/Vehicle.jsx
@@ -1,32 +1,37 @@
-
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchVehicles } from "../../redux/vehicleSlice";
+import { fetchVehiclesByBrandThunk } from "../../redux/vehicleSlice";
 import Filter from "../../components/serviceCard/ServiceCard";
 import VehicleCard from "../../components/VehicleCard/VehicleCard";
 
-const Vehicle = () => {
+const Vehicle = ({ brandId }) => {
    const dispatch = useDispatch();
-   // const { vehicles, loading, error } = useSelector((state) => state.vehicle);
-
-   // useEffect(() => {
-   //    dispatch(fetchVehicles());
-   // }, [dispatch]);
-
-   const { vehicles, allLoading, allError } = useSelector((state) => state.vehicle);
+   const { vehicles, allLoading, allError, brandLoading, brandError } = useSelector((state) => state.vehicle);
 
    useEffect(() => {
-      dispatch(fetchVehicles());
-   }, [dispatch]);
+      if (brandId) {
+         dispatch(fetchVehiclesByBrandThunk(brandId));
+      } else {
+         dispatch(fetchVehicles());
+      }
+   }, [dispatch, brandId]);
+
+   const isLoading = brandId ? brandLoading : allLoading;
+   const error = brandId ? brandError : allError;
 
    return (
       <div className="flex flex-col lg:flex-row p-6 gap-6">
-         <div className="w-full lg:w-1/4">
-            <Filter onFilterChange={(filters) => console.log("Filter applied", filters)} />
-         </div>
-         <div className="w-full lg:w-3/4">
-            {allLoading && <p className="text-center">Loading vehicles...</p>}
-            {allError && <p className="text-center text-red-500">{allError}</p>}
+         {!brandId && (
+            <div className="w-full lg:w-1/4">
+               <Filter onFilterChange={(filters) => console.log("Filter applied", filters)} />
+            </div>
+         )}
+
+         <div className={`w-full ${!brandId ? "lg:w-3/4" : ""}`}>
+            {isLoading && <p className="text-center">Loading vehicles...</p>}
+            {error && <p className="text-center text-red-500">{error}</p>}
+
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                {vehicles.map((vehicle) => (
                   <VehicleCard key={vehicle._id} vehicle={vehicle} />
